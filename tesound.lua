89e5579f3e8cbc10a91fe0c10ec35d781b594427
japanoise <japanoise@users.noreply.github.com>
2019-08-03 17:10:42 +0100

    Patched to work with love 11

--- tesound.lua
+++ tesound.lua
@@ -1,5 +1,6 @@
 --- TEsound v1.3, a simplified sound system for Love 2D
 -- @author Ensayia (Ensayia@gmail.com) & Taehl (SelfMadeSpirit@gmail.com)
+-- Edited by chameleon to work with 11.2
 TEsound = {}        -- Namespace
 TEsound.channels = {}   -- This holds the currently playing sound channels
 TEsound.volumeLevels = {} -- Volume levels that multiply the volumes of sounds with those tags
@@ -10,11 +11,12 @@ TEsound.pitchLevels = {}  -- Pitch levels that multiply the pitches of sounds wi
 
 --- Play a sound, or a random sound from a list. (with optional tag(s), volume, pitch, and on-finished function)
 -- @param sound A filepath to the sound file (example: "sounds/jump.ogg"), or a list of filepaths. If a list is used, a random sound from that list will be played. Passing a string is shorthand for passing a table with only one item.
+-- @param sourceType "stream" or "static"; use the latter for small files, the former for music.
 -- @param tags One or more tags that can be used to identify a sound (Example, single: "music". Example, multiple: {"sfx", "gun", "player"}). Multiple sounds may share tags, or tags may be unique. If omitted, no tags are assigned. Passing a string is shorthand for passing a table with only one item.
 -- @param volume A number between 0 and 1 which specifies how loud a sound is. If the sound has a tag which a volume has been specified for, it will multiply this number (ie., if you use the tag "sfx" and volume 0.5, and "sfx" has been tagVolume-ed to 0.6, then the sound will be played at 0.3 volume).
 -- @param pitch A number which specifies the speed/pitch the sound will be played it. If the sound has a tag which a pitch has been specified for, it will multiply this number.
 -- @param func A function which will be called when the sound is finished playing (it's passed one parameter - a list with the sound's volume and pitch). If omitted, no function will be used.
-function TEsound.play(sound, tags, volume, pitch, func)
+function TEsound.play(sound, sourceType, tags, volume, pitch, func)
   if type(sound) == "table" then
     assert(#sound > 0, "The list of sounds must have at least one sound.")
     sound = sound[love.math.random(#sound)]
@@ -22,8 +24,11 @@ function TEsound.play(sound, tags, volume, pitch, func)
   if not (type(sound) == "string" or (type(sound) == "userdata" and sound:type() == "SoundData")) then
     error("You must specify a sound - a filepath as a string, a SoundData, or a table of them. Not a Source!")
   end
+  if sourceType == nil or not (type (sourceType) == "string") then
+     error ("In version 11, you must provide a sourceType (\"stream\" or \"static\")")
+  end
 
-  table.insert(TEsound.channels, { love.audio.newSource(sound), func, {volume or 1, pitch or 1}, tags=(type(tags) == "table" and tags or {tags}) })
+  table.insert(TEsound.channels, { love.audio.newSource(sound, sourceType), func, {volume or 1, pitch or 1}, tags=(type(tags) == "table" and tags or {tags}) })
   local s = TEsound.channels[#TEsound.channels]
   s[1]:play()
   s[1]:setVolume( (volume or 1) * TEsound.findVolume(tags) * (TEsound.volumeLevels.all or 1) )
@@ -33,13 +38,14 @@ end
 
 --- Play a repeating sound, or random sounds from a list. (with optional tag(s), volume, and pitch)
 -- @param sound See TEsound.play
+-- @param sourceType "stream" or "static"; use the latter for small files, the former for music.
 -- @param tags See TEsound.play
 -- @param n The number of times the sound will be looped. If omitted, the sound will loop until TEsound.stop is used.
 -- @param volume See TEsound.play
 -- @param pitch See TEsound.play
-function TEsound.playLooping(sound, tags, n, volume, pitch)
-  return TEsound.play( sound, tags, volume, pitch,
-    (not n or n > 1) and function(d) TEsound.playLooping(sound, tags, (n and n-1), d[1], d[2]) end
+function TEsound.playLooping(sound, sourceType, tags, n, volume, pitch)
+  return TEsound.play( sound, sourceType, tags, volume, pitch,
+    (not n or n > 1) and function(d) TEsound.playLooping(sound, sourceType, tags, (n and n-1), d[1], d[2]) end
   )
 end
 
